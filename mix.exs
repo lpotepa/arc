c937c87265135dd738421a093ddda33567c59f18
Łukasz Potępa <lpotepa@o2.pl>
2019-11-23 19:58:13 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Arc.Mixfile do
       {:hackney, "~> 1.0"},
 
       # If using Amazon S3
-      {:ex_aws, "~> 2.0", optional: true},
+      {:ex_aws, "~> 2.1.0", optional: true},
       {:ex_aws_s3, "~> 2.0", optional: true},
       {:poison, "~> 2.2 or ~> 3.1", optional: true},
       {:sweet_xml, "~> 0.6", optional: true},
